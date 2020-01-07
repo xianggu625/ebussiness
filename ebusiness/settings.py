90e636c97983ae4f6bad0e9a8dbcb11da2719723
xianggu625 <xianggu625@126.com>
2020-01-07 18:25:59 +0800

    支持Django3.X

--- ebusiness/settings.py
+++ ebusiness/settings.py
@@ -118,18 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_URL = '/upload/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL= '/upload/'
 
 STATICFILES_FINDERS =(
 	"django.contrib.staticfiles.finders.FileSystemFinder",
 	"django.contrib.staticfiles.finders.AppDirectoriesFinder"
 )
 
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
 	os.path.join(BASE_DIR,"static"),
         os.path.join(BASE_DIR,"upload"),
-)
+]
 
 ALLOWED_HOSTS="*"
